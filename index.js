8a6179bcb1d98e6415b78f8948018fbbb86942eb
Mohammad815 <mohammadforhad176@gmail.com>
2021-10-27 19:36:59 +0600

    first

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 //middleWare
 app.use(cors())
 app.use(express.json())
